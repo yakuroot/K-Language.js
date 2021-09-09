d7d0df0f0a11510a6d826d630d6086f0a8344448
Neo <65282986+Neoration@users.noreply.github.com>
2021-09-10 05:30:33 +0900

    feat: add isHangul

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const josa = require("./src/josa");
+const { josa } = require("./src/josa");
 const {
   hasJongSeong,
   getSyllable,
 } = require("./src/syllable");
+const { isHangle } = require("./src/isHangul");
 
 module.exports = {
   josa,
   hasJongSeong,
   getSyllable,
+  isHangle,
 };
---
+++ src/isHangul.js
@@ -0,0 +1,47 @@
+module.exports = {
+  isHangle: function (str, option) {
+    if (
+      option !== undefined &&
+      option.removeSpace
+    )
+      str = str.replace(/(\s*)/g, "");
+      
+    var result = [];
+
+    for (var i = 0; i < str.length; i++) {
+      var uniChar = str.charCodeAt(i);
+
+      if (
+        (
+          uniChar >= 0x1100 &&
+          uniChar <= 0x11FF
+        ) ||
+        (
+          uniChar >= 0x3130 &&
+          uniChar <= 0x318F
+        ) ||
+        (
+          uniChar >= 0xAC00 &&
+          uniChar <= 0xD7A3
+        )
+      )
+        result.push(true);
+      else
+        result.push(false);
+    }
+
+    if (
+      option !== undefined &&
+      option.percent
+    )
+      return (result.filter((r) => r).length / str.length) * 100;
+    
+    if (
+      option !== undefined &&
+      option.toArray
+    )
+      return result;
+
+    return result.filter((r) => r).length === str.length;
+  },
+}
