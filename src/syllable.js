3e17331726a092e579174ef19bd25a88abebbe7f
Neo <65282986+Neoration@users.noreply.github.com>
2021-09-10 12:24:12 +0900

    feat: add isChoAble & isJongAble

--- src/syllable.js
+++ src/syllable.js
@@ -106,4 +106,52 @@ module.exports = {
       }, []);
     return result;
   },
+
+  isChoAble: function (str, option) {
+    var result = [];
+
+    for (var i = 0; i < str.length; i++)
+      result.push(
+        [
+          "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ",
+          "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ",
+          "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ",
+          "ㅋ", "ㅌ", "ㅍ", "ㅎ",
+        ]
+          .includes(str[i])
+      );
+
+    if (
+      option !== undefined &&
+      option.toArray
+    )
+      return result;
+
+    return result.filter((r) => r).length === str.length;
+  },
+
+  isJongAble: function (str, option) {
+    var result = [];
+
+    for (var i = 0; i < str.length; i++)
+      result.push(
+        [
+          "ㄱ", "ㄲ", "ㄳ", "ㄴ",
+          "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ",
+          "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ",
+          "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ",
+          "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ",
+          "ㅌ", "ㅍ", "ㅎ",
+        ]
+          .includes(str[i])
+      );
+
+    if (
+      option !== undefined &&
+      option.toArray
+    )
+      return result;
+
+    return result.filter((r) => r).length === str.length;
+  }
 }
