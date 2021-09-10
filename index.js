3bbc0a42b31b550b0c22c765ea1fb0cc6925ab45
Neo <65282986+Neoration@users.noreply.github.com>
2021-09-10 12:24:28 +0900

    feat: apply functions

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const { josa } = require("./src/josa");
 const {
   hasJongSeong,
   getSyllable,
+  isChoAble,
+  isJongAble,
 } = require("./src/syllable");
-const { isHangul } = require("./src/isHangul");
+const {
+  isHangul,
+  isCombined,
+} = require("./src/hangul");
 
 module.exports = {
   josa,
   hasJongSeong,
   getSyllable,
+  isChoAble,
+  isJongAble,
   isHangul,
+  isCombined,
 };
