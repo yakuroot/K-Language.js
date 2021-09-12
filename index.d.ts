466438756860abd4fa4f140fe8f257c4d233d057
Neo <65282986+Neoration@users.noreply.github.com>
2021-09-12 09:47:46 +0900

    feat: apply types

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ export type JosaTypes =
   `이${"나" | "란" | "든가" | "든지" | "나마" | "네"}`;
 
 declare interface Josa {
-  josa: (target: string, postposition: JosaTypes, option?: { preserve: boolean }) => string;
+  josa: (target: string, postposition: JosaTypes, options?: { preserve: boolean }) => string;
 }
 
 export type SyllableTypes = `${"초" | "중" | "종"}성` | "모두" | `${"cho" | "jung" | "jong"}${"seong" | ""}` | "all";
@@ -17,12 +17,21 @@ export interface SyllableOptions {
 
 declare interface HasJongSeong {
   hasJongSeong: (str: string) => boolean;
-  hasJongSeong: (str: string, option?: { toArray: true }) => boolean[];
+  hasJongSeong: (str: string, options?: { toArray: true }) => boolean[];
 }
 
 declare interface GetSyllable {
-  getSyllable: (str: string, option?: SyllableOptions) => string[][];
-  getSyllable: (str: string, option?: SyllableOptions & { toSeparateArray: true }) => string[];
+  getSyllable: (str: string, options?: SyllableOptions) => string[][];
+  getSyllable: (str: string, options?: SyllableOptions & { toSeparateArray: true }) => string[];
+}
+
+export interface AssemblesOptions {
+  includeOtherLng?: boolean;
+}
+
+declare interface GetAssembles {
+  getAssembles: (str: string[], options?: AssemblesOptions) => string[];
+  getAssembles: (str: string[], options?: AssemblesOptions & { toString: true }) => string;
 }
 
 export interface HangulOptions {
@@ -31,19 +40,36 @@ export interface HangulOptions {
 }
 
 declare interface IsHangul {
-  isHangul: (str: string, option?: HangulOptions) => boolean;
-  isHangul: (str: string, option?: HangulOptions & { percent: true }) => number;
-  isHangul: (str: string, option?: HangulOptions & { toArray: true }) => boolean[];
+  isHangul: (str: string, options?: HangulOptions) => boolean;
+  isHangul: (str: string, options?: HangulOptions & { percent: true }) => number;
+  isHangul: (str: string, options?: HangulOptions & { toArray: true }) => boolean[];
 }
 
 declare interface IsChoAble {
   isChoAble: (str: string) => boolean;
-  isChoAble: (str: string, option: { toArray: true }) => boolean[];
+  isChoAble: (str: string, options: { toArray: true }) => boolean[];
+}
+
+declare interface IsJungAble {
+  isJungAble: (str: string) => boolean;
+  isJungAble: (str: string, options: { toArray: true }) => boolean[];
 }
 
 declare interface IsJongAble {
   isJongAble: (str: string) => boolean;
-  isJongAble: (str: string, option: { toArray: true }) => boolean[];
+  isJongAble: (str: string, options: { toArray: true }) => boolean[];
+}
+
+declare interface IsConsonant {
+  isConsonant: (str: string) => boolean;
+  isConsonant: (str: string, options: { percent: true }) => number;
+  isConsonant: (str: string, options: { toArray: true }) => boolean[];
+}
+
+declare interface IsVowel {
+  isConsonant: (str: string) => boolean;
+  isConsonant: (str: string, options: { percent: true }) => number;
+  isConsonant: (str: string, options: { toArray: true }) => boolean[];
 }
 
 export interface KoreanNumberOptions {
@@ -53,28 +79,47 @@ export interface KoreanNumberOptions {
 }
 
 declare interface GetKoreanNumber {
-  getKoreanNumber: (num: number, option?: HangulOptions) => string[];
-  getKoreanNumber: (num: number, option?: HangulOptions & { toString: true }) => string;
+  getKoreanNumber: (num: number, options?: HangulOptions) => string[];
+  getKoreanNumber: (num: number, options?: HangulOptions & { toString: true }) => string;
 }
 
 declare const josa: Josa;
 declare const hasJongSeong: HasJongSeong;
 declare const getSyllable: GetSyllable;
-declare const isChoAble: IsChoAble;
-declare const isJongAble: IsJongAble;
+declare const getAssembles: GetAssembles;
+
 declare const isHangul: IsHangul;
-declare const getKoreanNumber: GetKoreanNumber;
 declare const search: (str: string, criteria: string) => boolean;
 declare const searchIndex: (str: string, criteria: string) => number[][];
 
+declare const isChoAble: IsChoAble;
+declare const isJungAble: IsJungAble;
+declare const isJongAble: IsJongAble;
+
+declare const isConsonant: IsConsonant;
+declare const isVowel: IsVowel;
+
+declare const getKoreanNumber: GetKoreanNumber;
+
+
+
 export default {
   josa,
+
   hasJongSeong,
   getSyllable,
-  isChoAble,
-  isJongAble,
+  getAssembles,
+
   isHangul,
   search,
   searchIndex,
+
+  isChoAble,
+  isJungAble,
+  isJongAble,
+
+  isConsonant,
+  isVowel,
+
   getKoreanNumber,
 };
