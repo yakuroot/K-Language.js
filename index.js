bd14989b8ef9d6366e5bd136f493cf7419239426
Neo <65282986+Neoration@users.noreply.github.com>
2021-09-12 09:41:43 +0900

    feat: apply all functions

--- index.js
+++ index.js
@@ -2,24 +2,69 @@ const { josa } = require("./src/josa");
 const {
   hasJongSeong,
   getSyllable,
-  isChoAble,
-  isJongAble,
+  getAssembles,
 } = require("./src/syllable");
 const {
   isHangul,
   search,
   searchIndex,
+  isChoAble,
+  isJungAble,
+  isJongAble,
+  isConsonant,
+  isVowel,
 } = require("./src/hangul");
 const { getKoreanNumber } = require("./src/number");
 
-module.exports = {
-  josa,
-  hasJongSeong,
-  getSyllable,
-  isChoAble,
-  isJongAble,
-  isHangul,
-  search,
-  searchIndex,
-  getKoreanNumber,
-};
+if (typeof define === "function" && define.amd) {
+  define(function () {
+    return {
+      josa,
+      hasJongSeong,
+      getSyllable,
+      getAssembles,
+      isHangul,
+      search,
+      searchIndex,
+      isChoAble,
+      isJungAble,
+      isJongAble,
+      isConsonant,
+      isVowel,
+      getKoreanNumber,
+    };
+  });
+} else if (typeof module !== undefined) {
+  module.exports = {
+    josa,
+    hasJongSeong,
+    getSyllable,
+    getAssembles,
+    isHangul,
+    search,
+    searchIndex,
+    isChoAble,
+    isJungAble,
+    isJongAble,
+    isConsonant,
+    isVowel,
+    getKoreanNumber,
+  };
+} else {
+  window.klng = {
+    josa,
+    hasJongSeong,
+    getSyllable,
+    getAssembles,
+    isHangul,
+    search,
+    searchIndex,
+    isChoAble,
+    isJungAble,
+    isJongAble,
+    isConsonant,
+    isVowel,
+    getKoreanNumber,
+  };
+}
+
